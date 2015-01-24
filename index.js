9db15fbaf1045772ace898cf3d902679dd2fb9bb
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-23 21:33:30 -0500

    faster implementation

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+module.exports = function(str) {
+  if (str.charCodeAt(0) === 46 /* . */ && str.indexOf('/', 1) === -1) {
+    return true;
+  }
 
-var dotfileRe = require('dotfile-regex');
-
-module.exports = function isDotfile(fp) {
-  return dotfileRe().test(fp);
+  var last = str.lastIndexOf('/');
+  return last !== -1 ? str.charCodeAt(last + 1) === 46  /* . */ : false;
 };
